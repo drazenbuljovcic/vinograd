af30eb638aeefd6a632764e4b3f2cdbbad7199f6
Dražen Buljovčić <dbuljovcic@execom.eu>
2017-01-19 21:38:35 +0100

    Minor

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import http from 'http';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-if(process.env.NODE_ENV !== 'production')
-  import reload from 'reload';
+import reload from 'reload';
   
 import i18n from 'i18n-abide';
 
