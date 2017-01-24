dd6e0cd09d70778e2013c03d1b237fb89ce568dd
Dražen Buljovčić <dbuljovcic@execom.eu>
2017-01-24 20:37:41 +0100

    Project structure

--- main.routes.js
+++ main.routes.js
@@ -6,8 +6,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index', { locales: i18n.getLocales(), currPath: req.path });
 })
-router.get('/about', (req, res) => {
-    res.render('about', { locales: i18n.getLocales(), currPath: req.path });
+router.get('/services', (req, res) => {
+    res.render('services', { locales: i18n.getLocales(), currPath: req.path });
 })
-
+router.get('/tourism', (req, res) => {
+    res.render('tourism', { locales: i18n.getLocales(), currPath: req.path });
+})
+router.get('/health', (req, res) => {
+    res.render('health', { locales: i18n.getLocales(), currPath: req.path });
+})
 export default router;
